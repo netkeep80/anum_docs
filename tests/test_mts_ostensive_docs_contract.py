f5cfc03accd19e37889a987244b5036717d426c3
Vertushkin Roman Pavlovich <netkeep80@gmail.com>
2026-08-09 23:18:44 +0300

    Сжатие МТС: объединить документацию постоянной апамяти
    * Сжать и русифицировать спецификацию апамяти
    
    * Удалить дублирующую спецификацию постоянной апамяти
    
    * Убрать дублирующую спецификацию L4 из активного набора
    
    * Расширить языковую защиту на документацию апамяти
    
    * Перевести ссылку ачисел на общую спецификацию апамяти

--- tests/test_mts_ostensive_docs_contract.py
+++ tests/test_mts_ostensive_docs_contract.py
@@ -9,9 +9,10 @@ README = ROOT / "README.md"
 FOUNDATIONS = ROOT / "docs" / "theory" / "Основания МТС.md"
 AXIOMS = ROOT / "docs" / "theory" / "Система аксиом МТС.md"
 NOTATION = ROOT / "docs" / "specs" / "Формальная нотация МТС.md"
+APAMEMORY = ROOT / "docs" / "specs" / "Апамять и управление сетью связей.md"
 
 ACTIVE_OSTENSIVE_DOCS = (README, FOUNDATIONS, AXIOMS, NOTATION)
-RUSSIAN_NORMATIVE_DOCS = (README, NOTATION)
+RUSSIAN_NORMATIVE_DOCS = (README, NOTATION, APAMEMORY)
 _ALLOWED_LATIN_PROSE = {
     "API",
     "Git",
