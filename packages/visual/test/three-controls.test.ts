e4080fb5cb1637cdc4126917e3a16bd33f8b38f9
Vertushkin Roman Pavlovich <netkeep80@gmail.com>
2026-08-23 19:50:48 +0300

    test(visual): add V2f-D missing API RED

---
+++ packages/visual/test/three-controls.test.ts
@@ -0,0 +1,13 @@
+import { setVisualThreeLivePhysicsOptions } from "../src/three/renderer.js";
+import {
+  createVisualThreeControlBar,
+  destroyVisualThreeControlBar,
+  getVisualThreeControlBarSnapshot,
+  toggleVisualThreeFullscreen,
+} from "../src/three/index.js";
+
+void setVisualThreeLivePhysicsOptions;
+void createVisualThreeControlBar;
+void destroyVisualThreeControlBar;
+void getVisualThreeControlBarSnapshot;
+void toggleVisualThreeFullscreen;
